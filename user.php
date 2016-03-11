9cf879d95169ed76178a7e27e5b37165221a4616
RanzTm <raintraagel@gmail.com>
2016-03-11 23:47:52 +0200

    Update user.php

--- user.php
+++ user.php
@@ -5,15 +5,16 @@
 	error_reporting(E_ALL);
 	$userData = api_user_read($_GET['id']);
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-print '
-  <div class="user">
-    <img class="img" src="'.$userData["profile_pic"].'" height="200" width="225">
-    <div class="col-name">Eesnimi: '.$userData['first_name'].'</div>
-	<div class="col-name">Perekonnanimi: '.$userData['last_name'].'</div>
-    <div class="col-username">Kasutajanimi: '.$userData['username'].'</div>
-    <div class="col-passwd">Kasutaja salasõna: '.$userData['password'].'</div>
-    <div class="col-info">Kommentaar: '.$userData['description'].'</div>
-  </div>
-';
 ?>
-	
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<meta charset="utf-8">	
+</head>
+  <div class="user">
+    <img class="img" src="<?=$userData["profile_pic"]?>" height="200" width="225">
+    <div class="col-name">Eesnimi:<?=$userData['first_name']?></div>
+	<div class="col-name">Perekonnanimi:<?=$userData['last_name']?></div>
+    <div class="col-username">Kasutajanimi:<?=$userData['username']?></div>
+    <div class="col-passwd">Kasutaja salasõna:<?=$userData['password']?></div>
+    <div class="col-info">Aeg: <?=$userData['date']?></div>
+  </div>
