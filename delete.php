c600232c346de9087a2a5f972fd3edeb5df162f0
RanzTm <raintraagel@gmail.com>
2016-03-14 15:53:32 +0200

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,23 @@
 <?php
-require_once('api.php');
-
-print '<a href="users.php">Edasi!</a>';
-$result = "";
-
-	if (api_user_delete($_GET["id"])) {
-		$result = "Kasutaja on kustutatud!";
-	}
-	
-	else {
-		$result = "Kasutaja kustutamisel tekkis viga!!";
-	}
-print '
-
-<div class="container" style="text-align: center">
-<h3>' . $result . '</h3>
-</div>';
+require_once("api.php");
+$item = user_API($_GET["key"]);
+remove_API($item);
 ?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+  <form role="form" action="index.php" method="get"><br><br>
+  
+  <p><b>Õpilane on eemaldatud!</b></p>
+  
+  <button type="submit" class="btn btn-default">Tagasi</button>
+  </form>
+</div>
+</body>
+</html>
