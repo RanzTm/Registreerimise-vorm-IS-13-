0f29a9c249d44136ac921a024229e541df8eb4a3
RanzTm <raintraagel@gmail.com>
2016-03-14 15:57:26 +0200

    Update users.php

--- users.php
+++ users.php
@@ -1,52 +1,44 @@
-<?php
-	print '<a href="form.html">Tagasi kasutaja registeerimise vormi lehe peale!</a>';
-	include('./bt.php');
-	require_once('api.php');
-?>
-
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="et">
 <head>
-    <title>Registreeritud kasutajad</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">	
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 
 
 <body>
-	<div class="container">
-	<div class="list-group col-sm-4 col-md-offset-4">
-    <h2>Registreeritud kasutajad</h2>
-			
-<?php
-$users = api_user_list();
-foreach ($users as $user) {
-	print '			
-	<div class="list-group-item">
-		<a href="user.php?id=' . $user["id"] . '">
-			<span class=""></span>
-			' . $user["username"] . ' 
-		</a>
-				
-	<span class="pull-right">							
-		<a href="edit.php?id=' . $user["id"] . '">
-			<button class="btn btn-primary">
-			<span class="glyphicon glyphicon-wrench"></span>
-				</button>
-		</a>
-		
-		<a href="delete.php?id=' . $user["id"] . '">
-			<button class="btn btn-info">
-			<span class="glyphicon glyphicon-remove-circle"></span>
-				</button>
-		</a>
-	</span>
-	</div>
-	';
-}
-?>
+<div class="collapse navbar-collapse" id="bs-navbar-collapse">
+	<ul class="nav navbar-nav">
+		<li><a href="form.html">Registreerimis vorm</a></li>
+	</ul>
+</div>
 
-	</div>
-    </div>
+<div class="container">
+<table align="left" cellpadding="5" rowspan="5">
+    <tr>
+        <th>Username</th>
+        <th>user</th>
+		<th>Edit</th>
+		<th>Remove</th>
+    </tr>
+	<?php
+	require_once("api.php");
+	$items = list_API();
+	
+	foreach ((array) $items as $key => $item) {
+		if ($item != NULL){
+	?>
+			<tr>
+				<td><?=$item["username"]?></td>
+				<td><a href="user.php?key=<?=$key?>"><?="user"?></a></td>
+				<td><a href="edit.php?key=<?=$key?>"><?="edit"?></a></td>
+				<td><a href="delete.php?key=<?=$key?>"><?="remove"?></a></td>
+			</tr>
+			<?php
+		}
+	}
+	?>
+</table>
+</div>
 </body>
 </html>
