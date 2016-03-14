6d7422752d2f15bed21d928fb7404a4d033c7ce2
RanzTm <raintraagel@gmail.com>
2016-03-14 15:56:29 +0200

    Update user.php

--- user.php
+++ user.php
@@ -1,21 +1,19 @@
 <?php
-	print '<a href="users.php">Tagasi registreeritud kasutajate lehe peale!</a>';
-	require_once ('api.php');
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
-	$userData = api_user_read($_GET['id']);
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+require_once("api.php");
+$item = user_API($_GET["key"]);
 ?>
+
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<meta charset="utf-8">
 	
 </head>
   <div class="user">
-    <img class="img" src="<?=$userData["profile_pic"]?>" height="200" width="225">
-    <div class="col-name">Eesnimi:<?=$userData['first_name']?></div>
-	<div class="col-name">Perekonnanimi:<?=$userData['last_name']?></div>
-    <div class="col-username">Kasutajanimi:<?=$userData['username']?></div>
-    <div class="col-passwd">Kasutaja salasõna:<?=$userData['password']?></div>
-    <div class="col-info">Aeg: <?=$userData['date']?></div>
+    <div class="col-name">Eesnimi:<?=$item['first_name']?></div>
+	<div class="col-name">Perekonnanimi:<?=$item['last_name']?></div>
+    <div class="col-username">Kasutajanimi:<?=$item['username']?></div>
+    <div class="col-passwd">Kasutaja salasõna:<?=$item['password']?></div>
+		<div class="col-name">Sugu:<?=$item['gender']?></div>
+			<div class="col-name">Kirjeldus:<?=$item['description']?></div>
+    <div class="col-info">Aeg: <?=$item['times']?></div>
   </div>
