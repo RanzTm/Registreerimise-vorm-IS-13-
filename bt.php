fc2713945657e99f2f375562588a0f5550cb6f2d
RanzTm <raintraagel@gmail.com>
2016-03-14 15:50:04 +0200

    Delete bt.php

--- bt.php
+++
@@ -1,5 +0,0 @@
-<?php
-	print '<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>';
-	print '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>';
-	print '<link  rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">';
-?>
