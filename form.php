a8a42cb59ebb976ea10400a00347cd5fe729708a
RanzTm <raintraagel@gmail.com>
2016-03-14 15:56:00 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,75 +1,55 @@
-<html lang="en">
+<?php
+require_once("api.php");
+$kontroll = $_POST["kontroll"];
+if ($kontroll == 0){
+	$item = array(
+	"id" => $_POST["id"],
+	"username" => $_POST["username"],
+	"password" => $_POST["password"],
+	"first_name" => $_POST["first_name"],
+	"last_name" => $_POST["last_name"],
+	"gender" => $_POST["gender"],
+	"description" => $_POST["description"],
+	"kontroll" => $_POST["kontroll"]);
+	
+	save_API($item);
+	}
+else {
+	$item = array(
+	"id" => $_POST["id"],
+	"username" => $_POST["username"],
+	"password" => $_POST["password"],
+	"first_name" => $_POST["first_name"],
+	"last_name" => $_POST["last_name"],
+	"gender" => $_POST["gender"],
+	"description" => $_POST["description"],
+	"kontroll" => $_POST["kontroll"]);
+	
+	edit_API($item);
+	}
+?>
+<!DOCTYPE html>
+<html lang="et">
 <head>
-	<title>Registreerimise Vorm 2015</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 </head>
 <body>
-	<div class="container">
-	<h4>Kasutaja registreerimise vorm</h4>
-	<form class="form-horizontal" role="form" action="form.php" method="post">
-
-	
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="usr">Kasutajanimi:</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="username" name="username">
-			</div>
-		</div>
-
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Kasutaja salasõna:</label>
-			<div class="col-sm-10">
-				<input type="password" class="form-control" id="password" name="password">
-			</div>
-		</div>
-
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="fname">Eesnimi:</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="first_name" name="first_name">
-			</div>
-		</div>
-
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="lname">Perekonnanimi:</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="last_name" name="last_name">
-			</div>
-		</div>
-
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="gender">Sugu:</label>
-			<div class="col-sm-10">
-				<input type="radio" id="gender" name="gender" value="Mees">Mees<br>
-				<input type="radio" id="gender" name="gender" value="Naine">Naine
-			</div>
-		</div>
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="description">Kommentaar:</label>
-			<div class="col-sm-10">
-				<textarea class="form-control" rows="5" id="description" name="description"></textarea>
-			</div>
-		</div>
-
-
-		
-		<div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
-				<input type="hidden" id="kontroll" name="kontroll" value=0>
-				<button type="submit" class="btn btn-default">Saada</button>
-				
-			</div>
-		</div>
-		
-	</form>
-	</div>
-</body>
-</html>
+<?php
+if ($tuvasta == 0) {
+  print '<div class="container">
+           <form role="form" action="form.html" method="get"><br><br>
+             <p><b>Lisatud</b></p>
+             <button type="submit" class="btn btn-default">Tagasi</button>
+           </form>
+           </div>';
+}
+else {
+  print '<div class="container">
+           <form role="form" action="user.php" method="get"><br><br>
+             <p><b>Muudetud!</b></p>
+             <button type="submit" class="btn btn-default">Tagasi</button>
+           </form>
+         </div>';
+}
+?>
