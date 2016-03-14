ba1b63a1fc0cb850f3c6d4051a33ebbe477865c4
RanzTm <raintraagel@gmail.com>
2016-03-14 15:54:32 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,38 +1,35 @@
 <?php
-	print '<a href="users.php">Tagasi registreeritud kasutajate lehe peale!</a>';
-	require_once('api.php');
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
-	$userData = api_user_read($_GET['id']);
+require_once("api.php");
+$item = user_API($_GET["key"]);
 ?>
 
 
 <html lang="en">
 <head>
-	<?php include('bt.php'); ?>
 	<title>Andmete muutmise vorm</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
 </head>
 <body>
 	<div class="container">
-	<h2>Kasutaja andmete muutmine</h2>
-	<form class="form-horizontal" role="form" method="post" action="form.php?id=<?php print $userData["id"]; ?>" enctype="multipart/form-data">
+	<h4>Kasutaja registreerimise vorm</h4>
+	<form class="form-horizontal" role="form" method="post" action="form.php" enctype="multipart/form-data">
 
 	
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="usr">Kasutajanimi:</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="usr" name="username" value="<?php print $userData["username"]; ?>" placeholder="Kasutajanimi">
+				<input type="text" class="form-control" id="usr" name="username" placeholder="kasutaja" value="<?=$item["username"]?>">
 			</div>
 		</div>
 
 		
 		<div class="form-group">
-			<label class="control-label col-sm-2" for="pwd">Salasõna:</label>
+			<label class="control-label col-sm-2" for="pwd">Kasutaja salasõna:</label>
 			<div class="col-sm-10">
-				<input type="password" class="form-control" id="pwd" name="password" value="<?php print $userData["password"]; ?>" placeholder="salasõna">
+				<input type="password" class="form-control" id="pwd" name="password" placeholder="salasõna" value="<?=$item["password"]?>">
 			</div>
 		</div>
 
@@ -40,7 +37,7 @@
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="fname">Eesnimi:</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="fname" name="first_name" value="<?php print $userData["first_name"]; ?>" placeholder="Eesnimi">
+				<input type="text" class="form-control" id="fname" name="first_name" placeholder="nimi" value="<?=$item["first_name"]?>">
 			</div>
 		</div>
 
@@ -48,7 +45,7 @@
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="lname">Perekonnanimi:</label>
 			<div class="col-sm-10">
-				<input type="text" class="form-control" id="lname" name="last_name" value="<?php print $userData["last_name"]; ?>" placeholder="Perenimi">
+				<input type="text" class="form-control" id="lname" name="last_name" placeholder="Perenimi" value="<?=$item["last_name"]?>">
 			</div>
 		</div>
 
@@ -56,7 +53,7 @@
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="gender">Sugu:</label>
 			<div class="col-sm-10">
-				<input type="radio" id="gender" name="gender" <?php print ($userData["gender"] == "Mees" ? "checked" : ""); ?> required>Mees
+				<input type="radio" id="gender" name="gender" value="Mees">Mees
 			</div>
 		</div>
 		
@@ -64,31 +61,25 @@
 		<div class="form-group">
 			<label class="control-label col-sm-2" for="gender"></label>
 			<div class="col-sm-10">
-				<input type="radio" id="gender" name="gender" <?php print ($userData["gender"] == "Naine" ? "checked" : ""); ?> required>Naine
+				<input type="radio" id="gender" name="gender" value="Naine">Naine
 			</div>
 		</div>
 
 		
 		<div class="form-group">
-			<label class="control-label col-sm-2" for="description">Info:</label>
+			<label class="control-label col-sm-2" for="description">Kommentaar:</label>
 			<div class="col-sm-10">
-				<textarea class="form-control" rows="5" id="description" name="description"><?php print $userData["description"]; ?></textarea>
+				<textarea class="form-control" rows="5" id="description" name="description" value="<?=$item["description"]?>"></textarea>
 			</div>
 		</div>
 
-		
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="pic"></label>
-			<div class="col-sm-10">
-				<img src="<?php print $userData["profile_pic"]; ?>" height="150" width="150">
-				<input type="file" name="profile_pic" accept="image/*" id="pic">
-			</div>
-		</div>
 
 		
 		<div class="form-group">
 			<div class="col-sm-offset-2 col-sm-10">
+				<input type="hidden" id="kontroll" name="kontroll" value=1>
 				<button type="submit" class="btn btn-default">Saada</button>
+				
 			</div>
 		</div>
 		
