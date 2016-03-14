f1564c3a1b09d47e039351fdf1dd92de20fc22b2
RanzTm <raintraagel@gmail.com>
2016-03-14 15:52:56 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,167 +1,77 @@
 <?php
 date_default_timezone_set("Europe/Tallinn");
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// CREATE //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function api_user_create($userData) {
-    $username = $userData["username"];
-    $password = $userData["password"];
-    $first_name = $userData["first_name"];
-    $last_name = $userData["last_name"];
-    $gender = $userData["gender"];
-    $description = $userData["description"];
-    $profile_pic = $userData["profile_pic"];	
-	$date = $userData["date"];
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	if (!empty($username && $password && $first_name && $last_name && $gender && $description )) { 
-        $homePath = "./kasutajad";
-        $dirArray = glob($homePath . "/*", GLOB_ONLYDIR);
-		
-        if (count($dirArray) != 0) {
-            $currentMax = "";
-            foreach ($dirArray as $value) {
-			if (basename($value) > $currentMax)
-                {
-				$currentMax = basename($value);
-                }
-            }
-            $user_id = $currentMax + 1;
-        }
-		
-        else {
-            $user_id = 1;
-        }
-	$userPath = "$homePath/$user_id";
-	$imgPath = "$userPath/profile.jpg";
-	$dataPath = "$userPath/data.json";
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	if (!is_dir($userPath)) {
-	$old = umask(0);
-	mkdir($userPath, 0770, true);
-	umask($old);
-	}
-	
-	if (is_writable($userPath)) {
-		if (!empty($profile_pic)) {
-			move_uploaded_file($profile_pic, $imgPath);
-            }
-			
-			$timestamp = strtotime(str_replace('/', '-', $date));
-            if ($timestamp === false) {
-                $timestamp = "";
-            }
-	
-            $file = fopen($dataPath, "w");
-            $data[] = array(
-			"id" => $user_id,
-			"username" => $username,
-			"password" => $password,
-			"first_name" => $first_name,
-			"last_name" => $last_name,
-			"gender" => $gender,
-			"description" => $description,
-			"profile_pic" => $imgPath);
-		fwrite($file, json_encode($data));
-		fclose($file);
-        return true;
-        }
-        return false;
-    }
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// USER_LIST //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function api_user_list() {
-    $path = './kasutajad/*';
-    $userList = [];
-    $i = 0;
-	
-    foreach (glob($path, GLOB_ONLYDIR) as $dirPath) {
-        $dataPath = "$dirPath/data.json";
-        $json = file_get_contents($dataPath);
-        $userData = json_decode($json, true);
-        $userList[$i] = $userData[0];
-        $i++;
-    }
-    return $userList;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// READ //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function api_user_read($user_id) {
-    $dataPath = "./kasutajad/$user_id/data.json";
-    $json = file_get_contents($dataPath);
-    $userData = json_decode($json, true);
-    $userData[0]["date"] = strftime("%d/%m/%Y %H:%M", $userData[0]["date"]);
-    return $userData[0];
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// UPDATE //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function api_user_update($userData) {
-    $user_id = $userData["id"];
-    $username = $userData["username"];
-    $password = $userData["password"];
-    $first_name = $userData["first_name"];
-    $last_name = $userData["last_name"];
-    $gender = $userData["gender"];
-    $description = $userData["description"];
-    $profile_pic = $userData["profile_pic"];
-	$date = $userData["date"];
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	if ((!empty($user_id) || $user_id == 0) && !empty($username && $password && $first_name && $last_name && $gender && $description )) { 
-        $userPath = "./kasutajad/$user_id";
-        $imgPath = "$userPath/profile.jpg";
-        $dataPath = "$userPath/data.json";
-		
-        if (is_writable($userPath)) {
-            if (!empty($profile_pic)) {
-			move_uploaded_file($profile_pic, $imgPath);
-            }
-			
-			$timestamp = strtotime(str_replace('/', '-', $date));
-			if ($timestamp === false) {
-			$timestamp = "";
-			}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            $file = fopen($dataPath, "w");
-            $data[] = array(
-                "id" => $user_id,
-                "username" => $username,
-                "password" => $password,
-                "first_name" => $first_name,
-                "last_name" => $last_name,
-                "gender" => $gender,
-                "description" => $description,
-                "profile_pic" => $imgPath);
-            fwrite($file, json_encode($data));
-            fclose($file);
-        return true;
-        }
-        return false;
-    }
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		// DELETE //
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function api_user_delete($user_id) {
-    $path = "./kasutajad/$user_id";
-    if (is_dir($path)) {
-        $files = glob($path . '/*');
-        foreach ($files as $file) {
-		is_dir($file) ? api_user_delete($file) : unlink($file);
-        }
-        rmdir($path);
+  
+function save_API($item) {
+    $username = $item["username"];
+    $password = $item["password"];
+    $first_name = $item["first_name"];
+    $last_name = $item["last_name"];
+	$gender = $item["gender"];
+	$description = $item["description"];
+    $id = file_get_contents("key.txt");
+	$times = time();
+	$data = json_decode(file_get_contents("andmed.json"), 2);
+	$data[] = array(
+		"id" => $id,
+		"username" => $username,
+		"password" => $password,
+		"first_name" => $first_name,
+		"last_name" => $last_name,
+		"gender" => $gender,
+		"description" => $description,
+		"times" => $times);
+	file_put_contents("andmed.json", json_encode($data, JSON_PRETTY_PRINT));
+	$id = $id+1;
+	file_put_contents("key.txt", $id);
+	 
 	return true;
-    }
-    return false;
 }
+function user_API($key) {
+	$item = json_decode(file_get_contents("andmed.json"), true);
+	$item = $item[$key];
+	$item["times"] = strftime("%d/%m/%Y %H:%M", $item["times"]);
+	return $item;
+	
+}
+function edit_API($item) {
+	$id = $item["id"];
+    $username = $item["username"];
+    $password = $item["password"];
+    $first_name = $item["first_name"];
+    $last_name = $item["last_name"];
+	$gender = $item["gender"];
+	$description = $item["description"];
+	
+	$id = intval($id);
+	$times = time();
+	$data = json_decode(file_get_contents("andmed.json"), true);
+	$data[$id] = array(
+		"id" => $id,
+		"username" => $username,
+		"password" => $password,
+		"first_name" => $first_name,
+		"last_name" => $last_name,
+		"gender" => $gender,
+		"description" => $description,
+		"times" => $times);
+	file_put_contents("andmed.json", json_encode($data, JSON_PRETTY_PRINT));
+	return true;
+}
+function remove_API($id) {
+	$array1 = json_decode(file_get_contents("andmed.json"), true);
+	$array2 = $array1[$id];
+	$id_nr = intval($array2["id"]);
+	unset($array1[$id_nr]);
+	file_put_contents("andmed.json", json_encode($array1));
+	
+}
+function list_API() {
+	$items = json_decode(file_get_contents("andmed.json"), 2);
+	if(is_array($items)) {
+		foreach($items as $key => $item)
+		
+	return $items;
+	}
+}
+	
 ?>
